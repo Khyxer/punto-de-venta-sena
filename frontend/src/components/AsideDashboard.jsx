6495c55ef2fe7fd897077d8b35faaf04e5bb5744
Khyxer <go5luis5go@gmail.com>
2026-01-25 12:53:22 -0500

    Cambios en el aside del dashboard

--- frontend/src/components/AsideDashboard.jsx
+++ frontend/src/components/AsideDashboard.jsx
@@ -32,7 +32,7 @@ export const AsideDashboard = () => {
   };
 
   return (
-    <aside className="max-w-55 w-full border-primary-color border-r select-none">
+    <aside className="max-w-54 w-full border-primary-color border-r select-none">
       <header className="h-12 border-primary-color border-b p-1 gap-2 flex items-center justify-center select-none">
         <img
           src="/LogoMain.png"
@@ -47,11 +47,11 @@ export const AsideDashboard = () => {
         {configMenu().map((item, index) => (
           <div key={index}>
             {item.dropMenu ? (
-              <>
+              <div className="relative">
                 {/* Botón de Configuración colapsable */}
                 <button
                   onClick={() => toggleDropdown(item.name)}
-                  className={`w-full flex items-center justify-between gap-2 px-3 py-2 duration-100 cursor-pointer text-sm
+                  className={`w-full flex items-center justify-between gap-2 px-3 py-2 cursor-pointer text-sm
                     ${
                       isActive(item.path)
                         ? "bg-primary-color text-light-color rounded-lg"
@@ -63,36 +63,51 @@ export const AsideDashboard = () => {
                     <item.icon size={18} />
                     {item.name}
                   </div>
-                    <ChevronRight size={18} style={{ rotate: openDropdowns[item.name] ? "90deg" : "0deg" }} className="transition-transform duration-200" />
+                  <ChevronRight
+                    size={18}
+                    style={{
+                      rotate: openDropdowns[item.name] ? "90deg" : "0deg",
+                    }}
+                    className="transition-transform duration-200"
+                  />
                 </button>
 
-                {/* Sub-items del dropdown (más pequeños) */}
-                {openDropdowns[item.name] && (
-                  <div className="mt-1">
-                    {item.dropMenu.map((subItem, subIndex) => (
-                      <Link
-                        key={subIndex}
-                        to={subItem.path}
-                        className={`flex items-center gap-2 px-3 py-2 rounded-lg duration-100 ml-4 text-sm
+                {/* Cuadrado para la animación de colapsar y expandir */}
+                <div
+                  className="absolute w-full -bottom-10  duration-100 bg-white"
+                  style={{
+                    height: openDropdowns[item.name] ? "0%" : "100%",
+                  }}
+                ></div>
+
+                {/* Sub-items del dropdown */}
+                {/* Ya no debe ser un renderizado condicional porque si no el cuadrado de la animación no se muestra correctamente */}
+                {/* {openDropdowns[item.name] && ( */}
+                <div className="mt-1">
+                  {item.dropMenu.map((subItem, subIndex) => (
+                    <Link
+                      key={subIndex}
+                      to={subItem.path}
+                      className={`flex items-center gap-2 px-3 py-2 rounded-lg  ml-4 text-sm
                           ${
                             isActive(subItem.path)
                               ? "bg-primary-color text-light-color"
                               : "hover:bg-gray-200 text-gray-600 hover:text-black"
                           }
                         `}
-                      >
-                        <subItem.icon size={18} />
-                        {subItem.name}
-                      </Link>
-                    ))}
-                  </div>
-                )}
-              </>
+                    >
+                      <subItem.icon size={18} />
+                      {subItem.name}
+                    </Link>
+                  ))}
+                </div>
+                {/* )} */}
+              </div>
             ) : (
               /* Items normales sin dropdown */
               <Link
                 to={item.path}
-                className={`flex items-center gap-2 px-3 py-2 duration-100 text-sm rounded-lg
+                className={`flex items-center gap-2 px-3 py-2 text-sm rounded-lg
                   ${
                     isActive(item.path)
                       ? "bg-primary-color text-light-color"
