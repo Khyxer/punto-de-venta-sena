6f2460a9e0c179154c21572b69515737ee9aea6c
Khyxer <go5luis5go@gmail.com>
2026-02-06 21:47:10 -0500

    Menu de navegación para la ayuda

---
+++ frontend/src/constants/constAsideHelpNavMenu.js
@@ -0,0 +1,26 @@
+export const helpSections = [
+  {
+    id: 'crear-producto',
+    name: 'Crear producto',
+    category: 'Productos',
+    file: 'crear-producto.md'
+  },
+  {
+    id: 'editar-producto',
+    name: 'Editar producto',
+    category: 'Productos',
+    file: 'editar-producto.md'
+  },
+  {
+    id: 'eliminar-producto',
+    name: 'Eliminar producto',
+    category: 'Productos',
+    file: 'eliminar-producto.md'
+  },
+  {
+    id: 'gestionar-ordenes',
+    name: 'Gestionar órdenes',
+    category: 'Órdenes',
+    file: 'gestionar-ordenes.md'
+  }
+];
